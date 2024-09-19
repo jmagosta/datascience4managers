00e0e9d16dbdd1df49a3590fe8b6d9fc17d5ddbf
JohnMark <johnmark.agosta@gmail.com>
2024-09-19 13:08:27 -0700

    log trajectory to csv

--- Part_1a/c_server/main.py
+++ Part_1a/c_server/main.py
@@ -1,11 +1,13 @@
 # c_server/main.py 
-# 
-# c_server_es.py
+#
+# An interactive Bokeh webpage to run a sequential investment game 
+# and compare it to a set of Kelly-strategy trajectories. 
+# Derived from c_server_es.py
 #
 # To Run:
 # Part1a/ $ bokeh serve --show c_server --port=8090
 
-import os, sys
+import csv, os, sys
 from pathlib import Path
 import random
 import numpy as np
@@ -21,14 +23,17 @@ from bokeh.plotting import figure, curdoc
 from numpy.random import default_rng
 rng = default_rng(seed=142)
 
+### Constants
 INITIAL_WEALTH = 1E6
 DEFAULT_AT_RISK = 0.5
 SIMULATION_COUNT = 10                     # Number of simulated trajectories
-WIN_P = 0.55                               # Prob a wager will win
+WIN_P = 0.55                              # Prob a wager will win
 NUM_PERIODS = 12                          # Repetitions of the wager. 
 PKL_FILENAME = f'trajectories_{NUM_PERIODS}_by_{SIMULATION_COUNT}.pkl'
+LOG_FILE = f'dice_{WIN_P}.csv'
 EPS = 1E-1
 
+### The simulation 
 def kelly_rule(p):
     'The fraction to bet for an assumed win probability'
     # This works as a vectorized function 
@@ -41,6 +46,7 @@ def investment_process(true_win_p = WIN_P, rounds=NUM_PERIODS-1):
 def wealth_trajectory(win_p_estimate):
     """
     Use the estimated win probability update as the bet fraction, using the Kelly rule
+    to run a simulation for comparison with the player's trajectory.
     """
     wins = investment_process(win_p_estimate)
     periods = len(wins)
@@ -68,14 +74,37 @@ def run_simulations():
         trajectories_x[:, k], trajectories_y[:, k] = wealth_trajectory(WIN_P)
     return trajectories_x, trajectories_y
 
+# Track the user investment path; the trajectory
 def chosen_trajectory(invest_fraction, past_trajectory, win_or_lose):
-    'Compute the next increment of the investors wealth.'
+    'Compute the next increment of the investors wealth and append to past_trajectory.'
     delta_fraction = invest_fraction * (2 * win_or_lose -1)
     # print(f'\t {win_or_lose}, {delta_fraction}')
-    # x
-    past_trajectory['x'].append(past_trajectory['x'][-1] +1)
-    past_trajectory['y'].append(past_trajectory['y'][-1] * (1 + delta_fraction))
+    # 
+    past_trajectory['f'].append(invest_fraction)
+    past_trajectory['t'].append(past_trajectory['t'][-1] +1)  # 
+    past_trajectory['b'].append(int(delta_fraction * past_trajectory['w'][-1]))               # the bet result, either + or - 
+    past_trajectory['w'].append(int(past_trajectory['w'][-1] * (1 + delta_fraction)))
+    print('trajectory: ', past_trajectory)
     return past_trajectory
+
+### IO ########
+
+def create_log():
+    with open(LOG_FILE, 'w', newline='') as fd:
+        writer = csv.writer(fd)
+        writer.writerow([f'trial_{WIN_P}', 'invest_fraction', 'bet', 'wealth'])
+    return None
+
+def append_log(bet, invest_fraction, past_trajectory):
+    trial = past_trajectory['t'][-1]
+    bet = past_trajectory['b'][-1]
+    wealth = past_trajectory['w'][-1]
+    with open(LOG_FILE, 'a', newline='') as fd:
+        writer = csv.writer(fd)
+        writer.writerow([trial, invest_fraction, bet, wealth])
+    return None
+
+### Display #####
 
 
 def plot_simulations(past_trajectory, x,y, cycle):
@@ -87,7 +116,7 @@ def plot_simulations(past_trajectory, x,y, cycle):
     background_fill_color="#fafafa",
     x_axis_label = 'Trials',
     y_axis_label = 'Accumulated wealth')
-    p.x_range = Range1d(0, cycle)
+    p.x_range = Range1d(0, cycle+1)
     # p.y_range = Range1d(0, max(y[NUM_PERIODS-1,:]))
     p.xaxis.minor_tick_line_color = None
     p.xaxis.ticker = list(range(cycle+1))
@@ -95,115 +124,119 @@ def plot_simulations(past_trajectory, x,y, cycle):
         w_df = pd.DataFrame({'y': y[:,k], 'x': x[:,k]})
         sim_src = ColumnDataSource(w_df)
         p.step(x='x', y='y', source=sim_src, line_alpha=0.4, line_color='grey')
-    p.step(x=past_trajectory['x'], y=past_trajectory['y'], line_color='darkred', line_width=3)
-    return(p)
+    p.step(x=past_trajectory['t'], y=past_trajectory['w'], line_color='darkred', line_width=3)
+    return p
 
 ### Callbacks 
 def slider_handler(attr, old_v, new_v):
     'respond to changes in the slider value'
-    global fraction_at_risk, value_at_risk, ds
+    global fraction_at_risk, value_at_risk, bet 
     fraction_at_risk = float(new_v)
     # Display the dollar amount at risk
-    value_at_risk = int(fraction_at_risk *  past_trajectory['y'][-1])
-    ds.data['frac'] = [fraction_at_risk]
-    ds.data['value'] = [value_at_risk]
-    print('ds', ds.data['value'])
-    # print(f'value_at_risk {value_at_risk}')
+    value_at_risk = int(fraction_at_risk *  past_trajectory['w'][-1])
+    bet.data['frac'] = [fraction_at_risk]
+    bet.data['value'] = [value_at_risk]
+    # print('slider', bet.data['frac'], bet.data['value'])
 
 # create a callback that adds a number in a random location
 def button_callback():
     'Run the next investment cycle'
-    global investment_cycle, past_trajectory, a_layout, ds
+    global investment_cycle, past_trajectory, a_layout, bet
     investment_cycle +=1
     
-    print(f"fraction @ Button {fraction_at_risk:.2}, {ds.data['value']}\n")
-    past_trajectory = chosen_trajectory(fraction_at_risk, past_trajectory, rng.binomial(1, WIN_P, 1)[0])
+    print(f"fraction @ Button {fraction_at_risk:.2}, {bet.data['value']}")
+    win_p = rng.binomial(1, WIN_P, 1)[0]
+    past_trajectory = chosen_trajectory(fraction_at_risk, past_trajectory, win_p)
     x,y = run_simulations()
     trajectories = plot_simulations(past_trajectory, x,y, investment_cycle)
     # Update the value at risk given the new wealth
-    value_at_risk = int(fraction_at_risk *  past_trajectory['y'][-1]) 
-    ds.data['value'] = [value_at_risk]
+    # NOTE is is already set in the slider callback?
+    # value_at_risk = int(fraction_at_risk *  past_trajectory['w'][-1]) 
+    bet.data['value'] = [value_at_risk]
     new_layout = controls(trajectories, d, a_press, fraction_slider)
     a_layout.children = new_layout.children
-    print( f'{investment_cycle}: W=${past_trajectory['y'][-1]:.0f}')
+    print( f'Button {investment_cycle}: W=${past_trajectory['w'][-1]:.0f};{bet.data['value']}\n')
+    append_log(bet, fraction_at_risk, past_trajectory )
 
 # Called pretty much anytime something happens.
 # This is needed to force a re-plot. 
 def re_render(the_event):
     global a_layout, a_press, fraction_slider
-    print('render ds, frac, value: ', ds.data['frac'], ds.data['value'])
+    # print('render bet, frac, value: ', bet.data['frac'], bet.data['value'])
     pass
 
 def controls(trajectories, d, a_press, fraction_slider):
     new_layout = column(trajectories, row(fraction_slider, d,  a_press))
     return new_layout
 
-
-    
-
 ### MAIN #####################################################################
 
-investment_cycle = 1
-fraction_at_risk = DEFAULT_AT_RISK
-value_at_risk = int(INITIAL_WEALTH * DEFAULT_AT_RISK)
-# History of investment
-past_trajectory = dict(x=[0], y=[INITIAL_WEALTH])
-
+# Create widgets
 # ColumnDataSource takes lists as dictionary values
-ds = ColumnDataSource(data=dict(frac=[fraction_at_risk], value=[value_at_risk]))
+# Make data available to widgets. 
+bet = ColumnDataSource(data=dict(frac=[DEFAULT_AT_RISK], value=[DEFAULT_AT_RISK * INITIAL_WEALTH]))
+d = Div(text=f"<font size='10'> investment: {bet.data['value'][0]} </font>")
 
-d = Div(text=f"<font size='10'> investment: {ds.data['value'][0]} </font>")
-
-    # Create widgets
 fraction_slider = Slider(start=0, end=1.0, value=DEFAULT_AT_RISK, step=.01, title="Investment fraction", name='fraction_at_risk')
-fraction_slider.js_on_change("value", CustomJS(args = dict(d=d, ds=ds), code=""" 
-    var df = ds.data['value'][0];
-    console.log('ds =', df);
+fraction_slider.js_on_change("value", CustomJS(args = dict(d=d, bets=bet), code=""" 
+    var df = bets.data['value'][0];
+    console.log('bets =', df);
     d.text=  `<font size='10'>Investment: ${df}</font>`                      
      """))                            
     #                          CustomJS(code="""
     # console.log('fraction_slider: value=' + this.value, this.toString())                                           
 
+# NOTE slider handler updates globals fraction_at_risk and value_at_risk 
 fraction_slider.on_change("value", slider_handler)
-
-
 
 # the callback object cb_obj and callback data cb_data are available in each JS callback. 
 # Additionally, when using args callback attribute 
 # you can pass arbitrary number of additional objects
-# ds.js_on_change("data", CustomJS(args=dict(d=d), "d.text = `<font size='40'>Value: ${cb_obj.data['value'][0]}`;</font>"))
-# ds.js_on_change("value", CustomJS(args=dict(ds=ds),code= "console.log('ds ='  + ds"))
+# bets.js_on_change("data", CustomJS(args=dict(d=d), "d.text = `<font size='40'>Value: ${cb_obj.data['value'][0]}`;</font>"))
+# bets.js_on_change("value", CustomJS(args=dict(bets=bets),code= "console.log('bets ='  + bets"))
                                  
-
 # def update_var():
 #     ''
-#     global ds
-#     ds.data['value'][0] = [value_at_risk]
+#     global bets
+#     bets.data['value'][0] = [value_at_risk]
 
-# ds.on_change('data', update_var)
+# bets.on_change('data', update_var)
 
 # add a text renderer to the plot (no data yet)
 # r = p.text(x=[], y=[], text=[], text_color=[], text_font_size="26px",
 #            text_baseline="middle", text_align="center")
-# ds = r.data_source
+# bets = r.data_source
 
 # add a button widget and configure with the call back
 a_press = Button(label="Update investment")
 a_press.button_type ='danger'
 a_press.on_event('button_click', button_callback)
-a_press.js_on_click(CustomJS(args = dict(d=d, ds=ds), code=""" 
-    var df = ds.data['value'][0];
-    console.log('ds =', df);
+a_press.js_on_click(CustomJS(args = dict(d=d, bets=bet), code=""" 
+    var df = bets.data['value'][0];
+    console.log('bets =', df);
     d.text=  `<font size='10'>Bnvestment: ${df}</font>`                      
      """))  
 
-past_trajectory = chosen_trajectory(2*WIN_P-1, past_trajectory, rng.binomial(1, WIN_P,1)[0])
-x,y = run_simulations()
-trajectories = plot_simulations(past_trajectory, x,y, investment_cycle)
+### starting values
+investment_cycle = 1
+fraction_at_risk = DEFAULT_AT_RISK
+value_at_risk = int(INITIAL_WEALTH * DEFAULT_AT_RISK)
 
+# init
+create_log()
+# History of investment, with the first period a zero investment, for 
+# purposes of plotting.  
+past_trajectory = dict(f=[DEFAULT_AT_RISK, DEFAULT_AT_RISK] ,t=[0, investment_cycle], b=[0, value_at_risk], w=[INITIAL_WEALTH, INITIAL_WEALTH])
+append_log(bet, fraction_at_risk, past_trajectory )
+
+# past_trajectory = chosen_trajectory(bet.data['frac'][0], past_trajectory, rng.binomial(1, WIN_P,1)[0])
+x,y = run_simulations()
+trajectory_plot = plot_simulations(past_trajectory, x,y, investment_cycle)
+
+### Event handling 
 curdoc().on_change(re_render)
 # curdoc().js_on_event(DocumentEvent, CustomJS(code='console.log("JS:DocumentEvent")'))
 
 # put the button and plot in a layout and add to the document
-a_layout = controls(trajectories, d, a_press, fraction_slider)
+a_layout = controls(trajectory_plot, d, a_press, fraction_slider)
 curdoc().add_root(a_layout)
